be9e9c0bf1c8c5d9bf148a14906ee1ff65a1b934
이대규 <eorb0717@naver.com>
2023-12-28 10:34:20 +0900

    항해 3주차 테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // todoList의 state
+  const [todos, setTodos] = useState([{ id: 0, body: "react를 배워봅시다." }]);
+  // input value값 state 변경 감지
+  const [body, setBody] = useState("");
+  // input value handler
+  const inputValueHandler = (event) => {
+    setBody(event.target.value);
+  };
+  // 추가하기 버튼 클릭하여 todoList 추가
+  const addTodoListHandler = () => {
+    const newTodo = {
+      id: todos.length + 1,
+      body,
+    };
+    setTodos([...todos, newTodo]);
+    setBody("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      {/* header 파트 todoList 추가 */}
+      <div className="header">
+        <div className="header-function">
+          <input
+            className="header-input"
+            type="text"
+            onChange={inputValueHandler}
+            value={body}
+          />
+          <button className="header-btn" onClick={addTodoListHandler}>
+            추가하기
+          </button>
+        </div>
+
+        <h1 className="header-title">Todo List</h1>
+      </div>
+      {/* main todoList 렌더링 */}
+      <div className="list-container">
+        {todos.map((todo) => {
+          console.log("todo의 값 확인 =>", todo);
+          return (
+            <div key={todo.id} className="todo">
+              <div>{todo.body}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
